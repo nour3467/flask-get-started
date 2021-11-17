6f4a651b30c206faad34540dda7ae1c1e6781e49
noureddine ech-chouky <noureddineechchouky@gmail.com>
2021-11-17 16:26:55 +0100

    render_template not define fixed

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>This is a home page</h1>"
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return "<h1>This is an about page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
     app.run()
+
